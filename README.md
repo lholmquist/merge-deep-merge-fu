048f1e2c66c44f15b407f8319d9c1d310519ecdf
Lucas Holmquist <lholmqui@redhat.com>
2020-04-16 15:24:31 -0400

    chore: update readme with special things

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 When merge an object with a read stream, merge deep seems to keep it a stream,  but deepmerge doesn't?
+
+deep merge by default will copy properties, which makes "special" objects go away.  merge deep keeps special things by default.
+
+you can add a option to deep merge to keep special things
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const deepmerge = require('deepmerge');
 const mergedeep = require('merge-deep');
 const isStream = require('isstream');
+const isPlainObject = require('is-plain-object')
 
 const archivePath = '/home/lucasholmquist/develop/just-playing/merge-deep-merge-fu/archive.tar';
 const readStream = fs.createReadStream(archivePath);
@@ -18,7 +19,10 @@ const mergeDeeped = mergedeep({key1: 'merge-deep'}, options);
 
 console.log(mergeDeeped);
 
+const deepMergeWithPlainObject = deepmerge({key1: 'deepmergedPlainObject'}, options, {isMergeableObject: isPlainObject});
 
+console.log(deepMergeWithPlainObject);
 
 console.log(`deep merge stream check: ${isStream(deepMerged.body)}`);
+console.log(`deep merge with plain object stream check: ${isStream(deepMergeWithPlainObject.body)}`);
 console.log(`merge deep stream check: ${isStream(mergeDeeped.body)}`);
